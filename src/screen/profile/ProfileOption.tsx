41d5574618a6db773a4f12748cabdf1bfe2a1c16
= <=>
2021-06-05 12:56:57 +0530

    privacy & terms url added

--- src/screen/profile/ProfileOption.tsx
+++ src/screen/profile/ProfileOption.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { List } from 'react-native-paper';
+import { Linking } from 'react-native';
+import { Divider, List } from 'react-native-paper';
+
+const privacy_url = "https://www.privacypolicies.com/live/e05f2ba4-5565-4dd2-bfc3-5aa745191f38";
+const terms_url = "https://www.websitepolicies.com/policies/view/kb45CpoF";
 
 interface Props {
     openOngoingTrip(): void
@@ -7,13 +11,21 @@ interface Props {
 
 const ProfileOption = React.memo((props: Props) => {
 
+    const onPrivacyPolicyPress = () => {
+        Linking.openURL(privacy_url);
+    }
+    const onTermsConditionPress = () => {
+        Linking.openURL(terms_url);
+    }
+
     return (
         <List.Section>
             <List.Item
                 title="Ongoing Trip"
-                description="view details of currently joined trip."
+                description="View details of currently joined trip."
                 left={props => <List.Icon {...props} icon="car-sport-outline" />}
                 onPress={props.openOngoingTrip} />
+            <Divider/>
             {/*<List.Item
                 title="Previous Trip"
                 description="view all your previous trips."
@@ -29,15 +41,18 @@ const ProfileOption = React.memo((props: Props) => {
                         color={accent}
                         onValueChange={changeNotStatus} />
                 )}
-                onPress={changeNotStatus} />
+                onPress={changeNotStatus} />*/}
             <List.Item
                 title="Privacy policy"
+                description="Read privacy policy"
                 left={props => <List.Icon {...props} icon="shield-outline" />}
-                onPress={onPress} />
+                onPress={onPrivacyPolicyPress} />
+            <Divider />
             <List.Item
                 title="Term's and condition"
+                description="Read term's and conditions"
                 left={props => <List.Icon {...props} icon="reader-outline" />}
-                onPress={onPress} />*/}
+                onPress={onTermsConditionPress} />
         </List.Section>
     );
 });
